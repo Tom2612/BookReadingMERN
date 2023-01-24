440a09eca283576225974cc773d55068de3f7157
tom <tomcat-26@hotmail.co.uk>
2023-01-24 18:03:47 +0800

    created routes folder and bookRoutes.js

---
+++ backend/routes/bookRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+// get all books
+
+
+// get individual books
+
+
+// create a book
+
+
+// update a book
+
+
+// delete a book
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
+
+app.use(express.json())
 app.use(cors());
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+})
+
+app.get('/', (req, res) => {
+    res.json({mssg: 'getting'})
+})
 
 app.listen(4000, () => {
     console.log('listening on port 4000')
