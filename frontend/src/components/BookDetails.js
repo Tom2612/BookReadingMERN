7ad3b4aafe7b3277e38b847737cbea7e99f1597f
tom <tomcat-26@hotmail.co.uk>
2023-01-26 11:13:21 +0800

    Added deleteBook dispatch

--- frontend/src/components/BookDetails.js
+++ frontend/src/components/BookDetails.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useBookContext } from '../hooks/useBookContext';
 
 export default function BookDetails({ book }) {
+  const { dispatch } = useBookContext();
+
+  const handleClick = async () => {
+    const response = await fetch('http://localhost:4000/api/books/' + book._id, {
+      method: 'DELETE'
+    })
+    const json = await response.json();
+
+    if(response.ok) {
+      dispatch({type: 'DELETE_BOOK', payload: json});
+    }
+  }
 
   return (
     <div className='book-details'>
@@ -8,6 +21,7 @@ export default function BookDetails({ book }) {
         <p><strong>Pages: </strong>{book.pages}</p>
         <p><strong>Rating: </strong>{book.rating}</p>
         <p>{book.createdAt}</p>
+        <span onClick={handleClick}>Delete</span>
     </div>
   )
 }
