c1fc72fafe7caef7ead4949d7f2aaf1779f75fcf
tom <tomcat-26@hotmail.co.uk>
2023-01-26 09:41:56 +0800

    Finished route controllers

--- backend/controllers/bookControllers.js
+++ backend/controllers/bookControllers.js
@@ -1,23 +1,69 @@
 const mongoose = require('mongoose');
+const Book = require('../models/bookModel');
 
 const getBooks = async (req, res) => {
-    res.status(200).json({mssg: 'get all books'});
+    const books = await Book.find({}).sort({createdAt: -1});
+
+    res.status(200).json(books);
 }
 
 const getBook = async (req, res) => {
-    res.send('get one book');
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such book'});
+    }
+
+    const book = await Book.findById(id);
+
+    if (!book) {
+        return res.status(404).json({error: 'No such book'});
+    }
+
+    res.status(200).json(book);
 }
 
 const createBook = async (req, res) => {
-    res.send('create a book');
+    const { title, rating, pages } = req.body;
+
+    try {
+        const book = await Book.create({ title, rating, pages });
+        res.status(200).json(book);
+    } catch(e) {
+        res.status(404).json({error: e.message});
+    }
 }
 
 const updateBook = async (req, res) => {
-    res.send('update a book');
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such book'});
+    }
+
+    const book = await Book.findOneAndUpdate({_id: id}, {...req.body});
+
+    if(!book) {
+        return res.status(404).json({error: 'No such book'});
+    }
+
+    res.status(200).json(book);
 }
 
 const deleteBook = async (req, res) => {
-    res.send('delete a book');
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such book'});
+    }
+
+    const book = await Book.findOneAndDelete({_id: id});
+
+    if(!book){
+        return res.status(404).json({error: 'No such book'});
+    }
+
+    res.status(200).json(book);
 }
 
 module.exports = {
