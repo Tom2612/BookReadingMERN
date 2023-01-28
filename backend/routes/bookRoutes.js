bfdb73ae9470ca68112497f0622315c3bfcad6bd
tom <tomcat-26@hotmail.co.uk>
2023-01-28 15:49:10 +0800

    Created middleware to check for authorization

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getBooks, getBook, createBook, updateBook, deleteBook } = require('../controllers/bookControllers.js');
+const requireAuth = require('../middleware/requireAuth');
+
+// require auth for all routes
+router.use(requireAuth);
 
 // get all books
 router.get('/', getBooks)
